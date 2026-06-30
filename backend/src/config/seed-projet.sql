cfbaad8347631a6498c53b71011982f15e3f6b3a
antoineLAU <xab97300@gmail.com>
2026-06-30 15:31:26 +0200

    correction des ligne 3, 4, 5

--- backend/src/config/seed-projet.sql
+++ backend/src/config/seed-projet.sql
@@ -1,5 +1,5 @@
 INSERT INTO projet (id, createur_id, titre, description, date_creation, date_debut, date_fin, adresse, est_termine)
 VALUES 
-(1, 1, 'tralala', 'text', '2026-06-30', NULL, NULL, '69000 LYON', TRUE)
-(2, 2, 'tralala', 'text', '2026-06-30', NULL, NULL, '69000 LYON', TRUE)
-(3, 3, 'tralala', 'text', '2026-06-30', NULL, NULL, '69000 LYON', TRUE)
+(1, 1, 'tralala', 'text', '2026-06-30', NULL, NULL, '69000 LYON', TRUE),
+(2, 2, 'tralala', 'text', '2026-06-30', NULL, NULL, '69000 LYON', TRUE),
+(3, 3, 'tralala', 'text', '2026-06-30', NULL, NULL, '69000 LYON', TRUE);
