ff25f1155c7026479b13ee18a1265fdec9c2c27a
antoineLAU <xab97300@gmail.com>
2026-06-30 13:13:37 +0200

    modif du fichier

--- backend/src/config/seed_projet.sql
+++ backend/src/config/seed_projet.sql
@@ -1,6 +1,5 @@
 INSERT INTO projet (id, createur_id, titre, description, date_creation, date_debut, date_fin, adress, est_termine)
-VALUES
-(1, 'Marie',  'tralala', 'text', '0000-00-00', '0000-00-00', '0000-00-00', '69000 LYON', TRUE),
-(2, 'George', 'tralala', 'text', '0000-00-00', '0000-00-00', '0000-00-00', '69000 LYON', TRUE),
-(3, 'Paul',   'tralala', 'text', '0000-00-00', '0000-00-00', '0000-00-00', '69000 LYON', FALSE),
-(4, 'Gari',   'tralala', 'text', '0000-00-00', '0000-00-00', '0000-00-00', '69000 LYON', TRUE);
+VALUES (1, 'Marie', 'tralala', 'text', NULL, NULL, NULL, '69000 LYON', TRUE)
+ON DUPLICATE KEY UPDATE
+  titre = VALUES(titre),
+  description = VALUES(description);
