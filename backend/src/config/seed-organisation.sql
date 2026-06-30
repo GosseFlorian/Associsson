e603d6b59c822f279a101c71d1afae18a818952b
GosseFlorian <floriangosse91@gmail.com>
2026-06-30 12:06:20 +0200

    creation fichier seed-organisation pour alimenter la table organisation

---
+++ backend/src/config/seed-organisation.sql
@@ -0,0 +1,6 @@
+INSERT INTO organisation (id, nom, date_creation, est_actif, proprietaire_id)
+VALUES
+(1, 'Associsson Gisèle', '2026-06-30', TRUE, 1),
+(2, 'Associsson LOL', '2026-06-30', TRUE, 2),
+(3, 'Associsson LAU', '2026-06-30', TRUE, 3),
+(4, 'Associsson Overwatch', '2026-06-30', TRUE, 1);
