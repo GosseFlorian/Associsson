3b149e00b41a407288dcb34d4ca8cd85439aa3ed
antoineLAU <xab97300@gmail.com>
2026-06-30 12:33:27 +0200

    ajoute dossier config

---
+++ backend/src/config/seed_projet.sql
@@ -0,0 +1,6 @@
+INSERT INTO projet (id, createur_id, titre, description, date_creation, date_debut, date_fin, adress, est_termine)
+VALUES
+(1,'Marie','tralala','text','0000-00-00','0000-00-00','0000-00-00', '69000 LYON', 'true')
+(1,'George','tralala','text','0000-00-00','0000-00-00','0000-00-00', '69000 LYON', 'true')
+(1,'Paul','tralala','text','0000-00-00','0000-00-00','0000-00-00', '69000 LYON', 'false')
+(1,'Gari','tralala','text','0000-00-00','0000-00-00','0000-00-00', '69000 LYON', 'true')
