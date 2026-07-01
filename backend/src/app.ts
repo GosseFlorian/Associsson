bfa1bd9940f4b0c39a3b546c940c86b66e4d58fb
GosseFlorian <floriangosse91@gmail.com>
2026-07-01 14:24:02 +0200

    Creation client.ts pour lié la base donnée avec pg au backend modification de la constante PORT et import de la config dotenv

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,9 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ?? 3000;
 
 // Autorise ton frontend (qui tourne sur le port 5173 ou autre) à appeler cette API
 app.use(cors());
