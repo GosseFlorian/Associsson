d7d401ebb7f7bffcd07328a9d70953c5bf7e1dd1
GosseFlorian <floriangosse91@gmail.com>
2026-07-06 11:24:42 +0200

    suppression du test getAllUsers car séparer en architecture MVC + service/respository import et utilisation de la route utilisateur.route.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,37 +1,15 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import { Request, Response } from "express";
-import type { Utilisateur } from "./types";
-import { pool } from "./config/client";
+import utilisateurRoutes from "./routes/utilisateur.route";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-// Autorise ton frontend (qui tourne sur le port 5173 ou autre) à appeler cette API
 app.use(cors());
-
-// Permet à Express de lire le format JSON envoyé dans les requêtes
 app.use(express.json());
 
-// Route de test ultra-simple
-app.get("/", (req, res) => {
-  res.json({ message: "Le backend fonctionne parfaitement ! 🚀" });
-});
-
-app.get("/utilisateur", getAllUtilisateur);
-
-async function getAllUtilisateur(req: Request, res: Response) {
-  try {
-    const result = await pool.query<Utilisateur>(
-      "SELECT * FROM utilisateur ORDER BY id",
-    );
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Erreur serveur" });
-  }
-}
+app.use("/utilisateur", utilisateurRoutes);
 
 // Lancement du serveur
 app.listen(PORT, () => {
