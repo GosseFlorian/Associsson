374f2f397bc3e0cba9134276ebf0102a358403f0
antoineLAU <xab97300@gmail.com>
2026-07-08 12:11:15 +0200

    modif fichier post-projet

--- backend/src/routes/projets.routes.ts
+++ backend/src/routes/projets.routes.ts
@@ -7,8 +7,4 @@ const router = Router();
 router.get("/", getProjetsController);
 router.get("/:id", getProjetByIdController);
 router.post("/", postProjetController);
-<<<<<<< HEAD
-=======
-
->>>>>>> e7d09146ba55132efcdbb61b34ae53a9b46ccbb4
 export default router;
