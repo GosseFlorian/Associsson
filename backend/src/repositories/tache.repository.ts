26c268822b06db45f9bd02b172b32c66b3b50e95
rachida <fofanahawarachida@gmail.com>
2026-07-09 08:58:50 +0200

    feat: ajout du PUT tache

--- backend/src/repositories/tache.repository.ts
+++ backend/src/repositories/tache.repository.ts
@@ -19,3 +19,36 @@ export const getTacheByIdRepository = async (
 
   return result.rows[0]; // ✔ peut être undefined → cohérent avec ton modèle Utilisateur
 };
+
+export const putTacheRepository = async (
+  id: number,
+  data: Tache,
+): Promise<Tache | undefined> => {
+  const query = `
+    UPDATE tache
+    SET projet_id = $1,
+        titre = $2,
+        description = $3,
+        statut = $4,
+        priorite = $5,
+        date_echeance = $6,
+        assigne_a = $7
+    WHERE id = $8
+    RETURNING *;
+  `;
+
+  const values = [
+    data.projet_id,
+    data.titre,
+    data.description,
+    data.statut,
+    data.priorite,
+    data.date_echeance,
+    data.assigne_a,
+    id,
+  ];
+
+  const result = await pool.query<Tache>(query, values);
+
+  return result.rows[0]; 
+};
