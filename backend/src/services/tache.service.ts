63665e76bc9ae1cbad754a0c12f9294a2b4969de
GosseFlorian <floriangosse91@gmail.com>
2026-07-17 16:07:13 +0200

    mise a jour des services avec ajout de certaine logique métier comme la vérification des champs obligatoire et la vérification du format du mot de passe et de l'email pour l'utilisateur modification des promises à partir des interfaces (dans types.ts) en concéquence

--- backend/src/services/tache.service.ts
+++ backend/src/services/tache.service.ts
@@ -1,34 +1,37 @@
-import { 
+import {
   getTachesRepository,
   getTacheByIdRepository,
   postTacheRepository,
   putTacheRepository,
-  deleteTacheRepository
+  deleteTacheRepository,
 } from "../repositories/tache.repository";
-import { Tache } from "../types";
+import { Tache, TacheDetails } from "../types";
 
-export const getTachesService = async (): Promise<Tache[]> => {
+export const getTachesService = async (): Promise<TacheDetails[]> => {
   return await getTachesRepository();
 };
 
 export const getTacheIdService = async (
   id: number,
-): Promise<Tache | undefined> => {
+): Promise<TacheDetails | null> => {
   return await getTacheByIdRepository(id);
 };
 
 export const postTacheService = async (data: Tache): Promise<Tache> => {
+  // Validation du titre
+  if (!data.titre || data.titre.trim() === "") {
+    throw new Error("Le titre de la tâche est obligatoire");
+  }
   return await postTacheRepository(data);
 };
 
 export const putTacheService = async (
   id: number,
   data: Partial<Tache>,
-): Promise<Tache> => {
+): Promise<Tache | null> => {
   return await putTacheRepository(id, data);
 };
 
-
-export const deleteTacheService = async (id: number): Promise<Tache> => {
+export const deleteTacheService = async (id: number): Promise<Tache | null> => {
   return await deleteTacheRepository(id);
 };
