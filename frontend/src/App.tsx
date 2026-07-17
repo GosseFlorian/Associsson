76889d3cd1be201702fde5790b7dcbf603880d57
rachida <fofanahawarachida@gmail.com>
2026-07-17 15:51:10 +0200

    Mise à jour App.tsx sur feat/register

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,20 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { Organisation, Membre } from './pages/Pages';
 import  Registerpage from "./pages/registerPage";
+
 
 function App() {
   return (
-    <Routes>
-      {/* C'est ici que tu ajouteras tes futures pages, ex: <Route path="/board" element={<Board />} /> */}
-
-      {/* Page erreur 404 */}
-      <Route path="*" element={<h2>Page non trouvée 404 🔍</h2>} />
-
-       {/* register */}
+    <>
+      <Routes>
+        
+        <Route path="/organisations" element={<Organisation/>} />
+        <Route path="/organisations/membre" element={<Membre/>} />
+        <Route path="*" element={<h2>Page non trouvée 404 🔍</h2>} />
       <Route path="/register" element={< Registerpage />} />
-
-
-    </Routes>
+      </Routes>
+    </>
   );
 }
-
 export default App;
