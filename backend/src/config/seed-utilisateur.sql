ef4ec5ac4a8386659c6e391add339f9747fcfc32
GosseFlorian <floriangosse91@gmail.com>
2026-07-23 12:33:21 +0200

    suppression des fichiers seed par table pour les regrouper dans un seul et même fichier

--- backend/src/config/seed-utilisateur.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO utilisateur (id, nom, email, mot_de_passe, date_inscription) VALUES
-(1, 'Florian', 'florian@gmail.com', 'Mot2passeFlori@n', '2026-06-30'),
-(2, 'Benjamin', 'benjamin@gmail.com', 'Mot2passeBenj@min', '2026-06-30'),
-(3, 'Antoine', 'antoine@gmail.com', 'Mot2passe@ntoine', '2026-06-30');
