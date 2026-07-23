e6c59943ffbeff1e9fe1d5f8da85c7391c9cd0d1
GosseFlorian <floriangosse91@gmail.com>
2026-07-23 12:36:05 +0200

    creation de fichier Down pour RESET / DELETE les données fictive / tables dans la base de données pour remise à zéro

---
+++ backend/src/config/migrationDown.sql
@@ -0,0 +1,12 @@
+-- Reset/Delete les tables
+
+DROP TYPE statut CASCADE;
+DROP TYPE priorite CASCADE;
+DROP TYPE role CASCADE;
+
+DROP TABLE utilisateur CASCADE;
+DROP TABLE membre CASCADE;
+DROP TABLE organisation CASCADE;
+DROP TABLE projet CASCADE;
+DROP TABLE tache CASCADE;
+DROP TABLE inscription_projet CASCADE;
