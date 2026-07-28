01d95eeb31d23d54089e208119038195d2ab8b2b
antoineLAU <xab97300@gmail.com>
2026-07-28 12:53:08 +0200

    homepage V2 modifier plus simple

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "../style/pages/HomePage.css";
-import Logo from "../style/img/Logo.png";
 import Logo2 from "../style/img/Logo2.png";
+import Image from "../style/img/image.png";
 
 export function HomePage() {
   const [hidden, setHidden] = useState(true);
@@ -39,19 +39,23 @@ export function HomePage() {
 
       <main>
         <div id="accueil" className="Acceuil">
-          <div className="image">
-            <img src={Logo} alt="Logo" draggable="false"/>
-          </div>
-          <div className="texte">
-            <h3>Une application pour les remplacer toutes.</h3>
-            <br />
-            <p>
-              Faites travailler vos équipes sur une seule plateforme conçue pour gérer tout type de travail.
-            </p>
-          </div>
-          <div className="bouton">
-            <button className="btnC">Connection</button>
-            <button className="btnI">Inscription</button>
+         <div className="texte" style={{ position: "relative" }}>
+            <img
+              src={Image}
+              alt="Image"
+              draggable="false"
+              style={{ width: "100%", display: "block" }}
+            />
+            <div className="message">
+              <h3>Une application pour les remplacer toutes.</h3>
+              <p>
+                Faites travailler vos équipes sur une seule plateforme conçue pour gérer tout type de travail.
+              </p>
+              <div className="bouton">
+                <button className="btnC">Connexion</button>
+                <button className="btnI">Inscription</button>
+              </div>
+            </div>
           </div>
         </div>
       </main>
