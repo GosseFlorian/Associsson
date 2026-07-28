cbafde78219e755f6e152f25b92c49980870c5fb
GosseFlorian <floriangosse91@gmail.com>
2026-07-28 15:08:22 +0200

    Ajout de la fonction de déconnexion et ajout du champ token

--- frontend/src/stores/loginStore.ts
+++ frontend/src/stores/loginStore.ts
@@ -5,30 +5,33 @@ interface LoginStore {
   idUtilisateur: number | null;
   idMembre: number | null;
   role: string | null;
+  token: string | null;
 
   setIdUtilisateur: (id: number | null) => void;
   setIdMembre: (id: number | null) => void;
   setRole: (role: string | null) => void;
+  setToken: (token: string | null) => void;
+  deconnexion: () => void;
 }
 
 export const useLoginStore = create<LoginStore>()(
   persist(
     (set) => ({
-      idUtilisateur: 1,
+      idUtilisateur: null,
       idMembre: null,
       role: null,
+      token: null,
 
-      setIdUtilisateur: (id) =>
-        set({ idUtilisateur: id }),
+      setIdUtilisateur: (id) => set({ idUtilisateur: id }),
+      setIdMembre: (id) => set({ idMembre: id }),
+      setRole: (role) => set({ role }),
+      setToken: (token) => set({ token }),
 
-      setIdMembre: (id) =>
-        set({ idMembre: id }),
-
-      setRole: (role) =>
-        set({ role }),
+      deconnexion: () =>
+        set({ idUtilisateur: null, idMembre: null, role: null, token: null }),
     }),
     {
       name: "login-storage",
-    }
-  )
+    },
+  ),
 );
