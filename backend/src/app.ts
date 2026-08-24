9b54987773fb4e28ac3c9fc9de9bc5719c814f10
ben-25 <ben-2004@hotmail.fr>
2026-08-24 10:44:32 +0200

    ajout helmet et protection cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import helmet from 'helmet';
 import express from 'express';
 import cors from 'cors';
 import utilisateurRoutes from './routes/utilisateur.route';
@@ -10,7 +11,8 @@ import tacheRoutes from './routes/tache.route';
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-app.use(cors());
+app.use(helmet());
+app.use(cors({ origin: ['http://localhost:5173'] }));
 app.use(express.json());
 
 app.use('/utilisateur', utilisateurRoutes);
